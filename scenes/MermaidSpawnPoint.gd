243a45798d832af336cf2ccc43e5b730d98eace2
Rene Jotham Culaway <rcculaway@up.edu.ph>
2023-11-12 20:17:46 +0800

    Added debugging node for Mermaid

---
+++ scenes/MermaidSpawnPoint.gd
@@ -0,0 +1,8 @@
+@tool
+extends Marker2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if Engine.is_editor_hint():
+		var mermaid = preload("res://scenes/Fishes/Mermaid.tscn").instantiate()
+		add_child(mermaid)
